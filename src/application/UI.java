9be5b65bd2535ca83b326b5ca46e29a041faa291
andreMapa <andreomapa01@gmail.com>
2021-12-20 21:34:55 -0300

    Check and Checkmate system

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,7 +58,17 @@ public class UI {
 		printcapturedPieces(captured);
 		System.out.println("\n");
 		System.out.println("Turn : " + chessMatch.getTurn());
-		System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+		
+		if(!chessMatch.isCheckmate()) {
+			System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.isCheck()) {
+				System.out.println("CHECK!");
+			}
+		}
+		else {
+			System.out.println("CHECKMATE");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
